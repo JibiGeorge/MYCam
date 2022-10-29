9e21d62389dde9edc8113bf6c9639aceaeab4cd5
Jibi George <jibiyyan@gmail.com>
2022-10-29 14:13:28 +0530

    Home banner looping withing database

--- controllers/userController/userHomePageController.js
+++ controllers/userController/userHomePageController.js
@@ -1,10 +1,14 @@
 const categoryController = require('/Brototype/Week 8/MyCam/model/category')
+const imageSlider = require('/Brototype/Week 8/MyCam/model/sliderImageModel')
 
 const homePage = (req,res)=>{
     let userData = req.session.user
-    categoryController.getCategory().then((category)=>{
-        res.render('user/homePage',{admin:false,user:true,category,userData})
-    })    
+    imageSlider.showImages().then((SliderImage)=>{
+        categoryController.getCategory().then((category)=>{
+            res.render('user/homePage',{admin:false,user:true,category,userData,SliderImage})
+        }) 
+    })
+       
 }
 
 module.exports ={
