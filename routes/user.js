20415c7bf4c4b482cab5a11edcc5d8315720ec07
Jibi George <jibiyyan@gmail.com>
2022-10-30 13:09:29 +0530

    Product detail Page Design Complete

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userHomePageController = require('../controllers/userController/userHomePageController')
 const userLoginController = require('../controllers/userController/userLoginController')
+const categoryController = require('/Brototype/Week 8/MyCam/model/category')
 
 router.get('/',userHomePageController.homePage)
 
@@ -10,6 +11,16 @@ router.get('/signup',userLoginController.signUpPage)
 router.post('/userSignUp',userLoginController.doSignUp)
 router.post('/userLogin',userLoginController.doLogin)
 router.get('/logout',userLoginController.doLogout)
+
+
+
+
+router.get('/test',(req,res)=>{
+    let userData = req.session.user
+    categoryController.getCategory().then((category)=>{
+        res.render('user/productDetails',{admin:false,user:true,title:"Test",category,userData})
+    })
+})
 
 
 module.exports = router
