215a47f90359e28f6fa07b0857e85de75f737f97
Jibi George <jibiyyan@gmail.com>
2023-04-01 15:55:11 +0530

    User signUp OTP issue

--- controllers/userLoginController.js
+++ controllers/userLoginController.js
@@ -8,36 +8,37 @@ require('dotenv').config();
 
 const OTP = `${Math.floor(1000 + Math.random() * 9000)}`;
 const sendVerifyMail = async (name, email, id) => {
-    try {
-        const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 587,
-            secure: false,
-            requireTLS: true,
-            auth: {
-                user: process.env.VERIFICATION_MAIL_ID,
-                pass: process.env.VERIFICATION_MAIL_PASSWORD
-            }
-        });
+    return new Promise((resolve, reject) => {
+        try {
+            const transporter = nodemailer.createTransport({
+                host: 'smtp.gmail.com',
+                port: 587,
+                secure: false,
+                requireTLS: true,
+                auth: {
+                    user: process.env.VERIFICATION_MAIL_ID,
+                    pass: process.env.VERIFICATION_MAIL_PASSWORD
+                }
+            });
 
-        const mailOption = {
-            from: process.env.VERIFICATION_MAIL_ID,
-            to: email,
-            subject: "For Verification Mail",
-            // html: `<p> HI `+name+` , please click here to <a href="http://localhost:3000/verify?id='+userId+'"> Verify </a> your mail. </p> <h1>${OTP}</h1>` 
-            html: `<p> Hi, ` + name + ` , Welcome to <h1>MyCam</h1>. Your OTP for Verification is <h3>${OTP}</h3> </p>`
-        }
-        transporter.sendMail(mailOption, function (error, info) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log(OTP);
-                console.log("Email has been sent:- ", info.response);
+            const mailOption = {
+                from: process.env.VERIFICATION_MAIL_ID,
+                to: email,
+                subject: "For Verification Mail",
+                // html: `<p> HI `+name+` , please click here to <a href="http://localhost:3000/verify?id='+userId+'"> Verify </a> your mail. </p> <h1>${OTP}</h1>` 
+                html: `<p> Hi, ` + name + ` , Welcome to <h1>MyCam</h1>. Your OTP for Verification is <h3>${OTP}</h3> </p>`
             }
-        })
-    } catch (error) {
-        console.log(error.message);
-    }
+            transporter.sendMail(mailOption, function (error, info) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve({ status: true })
+                }
+            })
+        } catch (error) {
+            reject(error.message);
+        }
+    })
 }
 
 const loginPage = (req, res) => {
@@ -48,7 +49,6 @@ const signUpPage = (req, res) => {
 }
 const doSignUp = (req, res) => {
     let userData = req.session.user
-    console.log(req.body);
     const verified = 0;
     const { first_Name, last_Name, user_Name, user_Email, mobile_No, password } = req.body
     userLoginModel.doSignUp(
@@ -61,18 +61,19 @@ const doSignUp = (req, res) => {
             mobile_No,
             password
         }
-    ).then((response) => {
-        console.log(response);
+    ).then( async(response) => {
         if (response.userexist) {
             categoryController.getCategory().then((category) => {
                 res.render('user/userSignUpPage', { admin: false, user: true, category, errMsg: "Email Already Exist!", userData })
             })
         } else {
             let userId = response._id
-            sendVerifyMail(response.user_Name, response.user_Email, userId)
-            categoryController.getCategory().then((category) => {
-                res.render('user/verificationOTP', { admin: false, user: true, category, userData, userId })
-            })
+            const { status } = await sendVerifyMail(response.user_Name, response.user_Email, userId);
+            if (status) {
+                categoryController.getCategory().then((category) => {
+                    res.render('user/verificationOTP', { admin: false, user: true, category, userData, userId })
+                })
+            }
         }
     })
 }
